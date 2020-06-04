375a68467b45010b76ceae82682734864c260446
aminh07 <hemansu_aminh@yahoo.com>
2020-06-04 16:33:35 +1000

    removed irrelevant files

--- logic.sh
+++
@@ -1,6 +0,0 @@
-if [[ $(expr $1 % 2) -eq 0 ]]
-then
-	echo 'how proper'
-else
-	echo 'how improper' $1
-fi
--- read.sh
+++
@@ -1,8 +0,0 @@
-echo 'Enter adjective'
-read adjuct
-echo 'Enter noun'
-read noun
-echo 'Enter verb'
-read verb
-
-echo $adjuct $noun $verb
